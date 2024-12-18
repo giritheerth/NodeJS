adec5f006a047daf1a9a12585c813077c4a6428f
Theerthagiri Soundararajan <49114091+giritheerth@users.noreply.github.com>
2024-12-18 16:39:16 +1100

    temp

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -7,19 +7,45 @@ const app = express()
 // port
 const PORT = 8383
 
+let data ={
+    name: "Giri"
+}
+
+
+// website endpoints- for sending back html and they typically come when a user enters a url in a browser
 // interpret the incoming request for http verbs (method) path and routes
 // method informs the nature of the request and then route is further subdirectory
 app.get('/',(req,res)=> {
     // this is endpoint 1 - /
     console.log("I hit this endpoint!",req.method)
+    res.send(`
+        <body
+        style="background:white;
+        color:green;">
+            <h1>Data is: </h1>
+            <p>${JSON.stringify(data)}</p>
+        </body>
+    `)
     res.sendStatus(201)
     // 201 -created, 500 - internal server error , 404 - not found
-
 })
 
 app.get('/dashboard',(req,res)=>{
     console.log("I hit Dashboard endponint!")
-    res.send('Welcome!')
+    res.send('<h1>Dashboard</h1>')
+})
+// API endpoints- non visuals
+app.get('/api/data',(req,res)=>{
+    console.log('one for data!')
+    res.send(data)
+})
+
+// CRUD - create-post, read-get, update-put, delete-delete
+
+app.post('/api/data',(req,res)=>{
+    const newEntry= req.body
+    res.sendStatus(201)
+
 })
 
 
