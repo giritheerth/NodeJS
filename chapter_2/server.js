f135115dcdda0248cff91430dcb3bdb372c5bb29
Theerthagiri Soundararajan <49114091+giritheerth@users.noreply.github.com>
2024-12-18 16:16:37 +1100

    learn

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -1,9 +1,27 @@
+// address of this server is : URL-> http://localhost:8383
+// IP : 127.0.01
 // import express modeule
-const express = require('express')
+import express from 'express'
 // declare app
 const app = express()
 // port
 const PORT = 8383
+
+// interpret the incoming request for http verbs (method) path and routes
+// method informs the nature of the request and then route is further subdirectory
+app.get('/',(req,res)=> {
+    // this is endpoint 1 - /
+    console.log("I hit this endpoint!",req.method)
+    res.sendStatus(201)
+    // 201 -created, 500 - internal server error , 404 - not found
+
+})
+
+app.get('/dashboard',(req,res)=>{
+    console.log("I hit Dashboard endponint!")
+    res.send('Welcome!')
+})
+
 
 
 app.listen(PORT, ()=> console.log(`Server has started on : ${PORT}`))
